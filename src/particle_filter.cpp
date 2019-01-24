7db0aa97df85304f66db2ab50c60fc8ce1cbb40d
Joseph Piperakis <i.piperakis@gmail.com>
2019-01-24 03:02:13 +0100

    Set init to true

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -48,6 +48,8 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
     
     particles.push_back(particle);
   }
+
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
